f7e9c0f3484dd9824279f49a2f56acb458c3e09e
tuturyye <arthur.jaumet@gmail.com>
2024-08-14 18:10:07 +0200

    feat: ibm logo is working

--- src/Emulator.zig
+++ src/Emulator.zig
@@ -68,25 +68,32 @@ pub fn fetch(emu: *Emulator) error{MemoryOutOfBound}!u16 {
 }
 
 pub fn decodeAndExecute(emu: *Emulator, inst: u16) error{UnknownInstruction}!void {
+    std.debug.print("\nInstruction: {X} => ", .{inst});
+
     switch (inst >> 12) {
         0x0 => { // clear screen
             emu.display.pixels = [_]u1{0} ** Display.SIZE;
+            std.debug.print("clear screen", .{});
         },
         0x1 => {
             emu.pc = inst & 0x0FFF;
+            std.debug.print("jump to {X}", .{emu.pc});
         },
         0x6 => {
             const register = (inst & 0x0F00) >> 8;
             const value: u8 = @truncate(inst & 0x00FF);
             emu.variable_registers[register] = value;
+            std.debug.print("set register {X} to value {X}", .{ register, value });
         },
         0x7 => {
             const register = (inst & 0x0F00) >> 8;
             const value: u8 = @truncate(inst & 0x00FF);
             emu.variable_registers[register] += value;
+            std.debug.print("add value {X} to register {X}", .{ value, register });
         },
         0xA => {
             emu.index_register = inst & 0x0FFF;
+            std.debug.print("set index_register to {X}", .{emu.index_register});
         },
         0xD => {
             const spriteHeight = inst & 0x000F;
@@ -94,12 +101,14 @@ pub fn decodeAndExecute(emu: *Emulator, inst: u16) error{UnknownInstruction}!voi
             const y = emu.variable_registers[(inst & 0x00F0) >> 4] & (Display.HEIGHT - 1);
             const spriteIndex = emu.index_register;
 
+            std.debug.print("draw sprite {X} with height {d} at position (x, y) = ({d}, {d})", .{ spriteIndex, spriteHeight, x, y });
+
             var turnedOffPixel = false;
             var i: usize = 0;
             while (i < spriteHeight and i + y < Display.HEIGHT) {
                 const spriteLine = emu.memory[spriteIndex + i];
                 var j: u3 = 0;
-                while (j < 4 and j + x < Display.WIDTH) {
+                while (j <= 7 and j + x < Display.WIDTH) {
                     const bit: u1 = @truncate((spriteLine >> (7 - j)) & 1);
                     emu.display.pixels[x + j + (i + y) * Display.WIDTH] = emu.display.pixels[x + j + (i + y) * Display.WIDTH] ^ bit;
 
@@ -107,7 +116,12 @@ pub fn decodeAndExecute(emu: *Emulator, inst: u16) error{UnknownInstruction}!voi
                         turnedOffPixel = true;
                     }
 
-                    j += 1;
+                    const res: struct { u3, u1 } = @addWithOverflow(j, 1);
+                    if (res[1] == 1) {
+                        break;
+                    }
+
+                    j = res[0];
                 }
 
                 i += 1;
